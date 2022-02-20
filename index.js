e440d1b3c01f711233b20aeb4893dfdd64bd47dd
kaiba <kaibadash@gmail.com>
2022-02-20 21:32:26 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://pokosho.com/a/recaptcha/',
+    {'timeout': 10000, 'waitUntil':'load'}
+  );
+  await page.click('#submit');
+  const json = await page.content();
+  console.log(json);
+  await browser.close();
+})();
